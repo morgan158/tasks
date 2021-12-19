3a9fa66b1a02a3bca0d8ba5b20fb26e1bb1608b6
Svetlana Astapova <morgan159@mail.ru>
2021-12-20 03:23:06 +0500

    add Anograms

---
+++ src/Anograms.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Anograms {
+    static boolean isAnagram(String a, String b) {
+        if (a.length() != b.length()) return false;
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        for (int i = 0; i < a.length(); i++) {
+
+            if (!a.contains(b.charAt(i)+"") || !b.contains(a.charAt(i)+"")) {
+                return false;
+            }
+        }
+        int aInt = 0, bInt = 0;
+        for (int i = 0; i < a.length(); i++){
+            aInt += a.charAt(i);
+            bInt += b.charAt(i);
+        }
+
+        return aInt == bInt;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+
+}
